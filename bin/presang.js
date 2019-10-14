56158489a64b2f340764f3d24e3d9490b79b94e7
Vidar Eldøy <vidar@eldoy.com>
2019-10-14 06:09:30 +0200

    Added build script

--- bin/presang.js
+++ bin/presang.js
@@ -1,2 +1,74 @@
 #!/usr/bin/env node
-require('../lib/server')()
+const fs = require('fs')
+const path = require('path')
+const { server } = require('../main.js')
+
+const cmd = process.argv[2] || 'help'
+const commands = { build, help, serve: server }
+// const root = path.resolve(path.join(__dirname, '..' ))
+const root = process.cwd()
+console.log({ root })
+const dir = path.join(root, process.argv[3] || 'dist')
+console.log({ dir })
+
+const fn = commands[cmd]
+if (!fn) {
+  console.log(`Unknown command: '${cmd}'`)
+  process.exit(1)
+}
+fn()
+
+function help () {
+  console.log([
+    '\nPresang commands:\n',
+    'help - print this menu',
+    'serve - start the server',
+    'build - build a static app\n'
+  ].join('\n'))
+}
+
+async function build () {
+  function find (name) {
+    const names = fs.readdirSync(path.join(root, name))
+    const result = {}
+    for (const x of names) {
+      result[x.split('.')[0]] = require(path.join(root, name, x))
+    }
+    return result
+  }
+  if (fs.existsSync(dir)) {
+    console.log(`Directory '${dir}' already exists, please delete it or give another name`)
+    process.exit(1)
+  }
+
+  let pages
+  try {
+    pages = find('pages')
+  } catch (e) {}
+  console.log(pages)
+
+  if (!pages) {
+    console.log('No pages found')
+    process.exit(1)
+  }
+
+  let layouts
+  try {
+    layouts = find('layouts')
+  } catch (e) {
+    console.log(e)
+    layouts = [path.join(root, 'lib', 'layout')]
+  }
+  console.log(layouts)
+
+  fs.mkdirSync(dir)
+
+  for (const key in pages) {
+    const page = pages[key]
+    const html = await layouts[page.layout || 'default'](page)
+    if (html) {
+      fs.writeFileSync(path.join(dir, `${key}.js`), html)
+    }
+  }
+  console.log(`Files written to '${dir}'`)
+}
