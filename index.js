341a1fc252b6d74a9af92eb4542397b4adb7ab9f
Vidar Eldøy <vidar@eldoy.com>
2019-10-15 07:26:24 +0200

    Fixing weird bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const h = require('./lib/html.js')
 const { q, qa } = require('./lib/query.js')
-const server = require('./lib/server.js')
 const request = require('./lib/request.js')
-module.exports = { h, q, qa, server, request }
+const server = require('./lib/server.js')
+module.exports = { h, q, qa, request, server }
