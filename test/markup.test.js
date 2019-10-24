b88461050dee6e42acaa54da89b4d84e96e8ef36
Vidar Eldøy <vidar@eldoy.com>
2019-10-24 03:16:19 +0200

    Support deep pages

--- test/markup.test.js
+++ test/markup.test.js
@@ -23,26 +23,59 @@ const home = {
   }
 }
 
-
 const about = {
   title: 'about',
   render: async function about ($) {
-    return `<div>Home</div>`
+    return `<div>About</div>`
+  }
+}
+
+const deep = {
+  title: 'deep',
+  render: async function deep ($) {
+    return `<div>Deep</div>`
   }
 }
 
 const app = {
-  layouts: { default: layout },
-  pages: { home, about }
+  pages: {
+    layouts: {
+      default: layout
+    },
+    home,
+    about,
+    docs: {
+      deep
+    }
+  }
 }
 
 const $ = { app, req, res }
 
 describe('markup', () => {
+  beforeEach(() => {
+    req.pathname = '/'
+  })
   it('should load some HTML', async () => {
     const result = await markup(req, res)($)
     expect(result.split('\n').map(x => x.trim()).join('')).toBe(
       '<!doctype html><html><body><div>Home</div></body></html>'
+    )
+  })
+
+  it('should load some HTML', async () => {
+    req.pathname = '/about.html'
+    const result = await markup(req, res)($)
+    expect(result.split('\n').map(x => x.trim()).join('')).toBe(
+      '<!doctype html><html><body><div>About</div></body></html>'
+    )
+  })
+
+  it('should load some deep HTML', async () => {
+    req.pathname = '/docs/deep.html'
+    const result = await markup(req, res)($)
+    expect(result.split('\n').map(x => x.trim()).join('')).toBe(
+      '<!doctype html><html><body><div>Deep</div></body></html>'
     )
   })
 })
