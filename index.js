c3b014e5bb41291f1ffcf970d87ad4c7f68cd939
Vidar Eldøy <vidar@eldoy.com>
2020-01-11 22:56:10 +0100

    Move route from core

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const markup = require('./lib/markup.js')
 const server = require('./lib/server.js')
 const i18n = require('./lib/i18n.js')
-module.exports = { markup, server, i18n }
+const route = require('./lib/route.js')
+module.exports = { markup, server, i18n, route }
