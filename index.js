4f29300261a3cf98d6c77d0a2167092d8b82c501
Vidar Eldøy <vidar@eldoy.com>
2020-01-19 18:29:47 +0100

    Add dispatch and tools

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const markup = require('./lib/markup.js')
 const serve = require('./lib/serve.js')
 const i18n = require('./lib/i18n.js')
+const dispatch = require('./lib/dispatch.js')
 const route = require('./lib/route.js')
-module.exports = { markup, serve, i18n, route }
+const tools = require('./lib/tools.js')
+module.exports = { markup, serve, i18n, dispatch, route, tools }
