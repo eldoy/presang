6812245539916169ad08f82248a2fabf088c7cb7
Vidar Eldøy <vidar@eldoy.com>
2020-01-28 02:02:10 +0100

    Add tests for pager, fix routemap issue

--- test/pager.test.js
+++ test/pager.test.js
@@ -6,15 +6,45 @@ describe('pager', () => {
     delete process.env.WAVEORB_APP
   })
 
-  it('should find a page', async () => {
+  it('should find a page without routemap', async () => {
+    process.env.WAVEORB_APP = 'test/apps/app3'
+    const app = await loader()
+    const $ = { app }
+
+    let page = pager('about', $)
+    expect(page).toBeDefined()
+
+    page = pager('index', $)
+    expect(page).toBeDefined()
+
+    page = pager('notfound', $)
+    expect(page).toBeUndefined()
+
+    page = pager('no/index', $)
+    expect(page).toBeUndefined()
+
+    page = pager('no/om-oss', $)
+    expect(page).toBeUndefined()
+  })
+
+  it('should find a page with routemap', async () => {
     process.env.WAVEORB_APP = 'test/apps/app2'
     const app = await loader()
     const $ = { app }
+
     let page = pager('about', $)
     expect(page).toBeDefined()
+
     page = pager('index', $)
     expect(page).toBeDefined()
+
     page = pager('notfound', $)
     expect(page).toBeUndefined()
+
+    page = pager('no/index', $)
+    expect(page).toBeDefined()
+
+    page = pager('no/om-oss', $)
+    expect(page).toBeDefined()
   })
 })
