cd0562b309de396d07c03e4cbbccc2111e14803f
Vidar Eldøy <vidar@eldoy.com>
2021-02-11 16:43:55 +0100

    Add default actions

--- test/loader.test.js
+++ test/loader.test.js
@@ -18,4 +18,20 @@ describe('loader', () => {
     expect(typeof app).toBe('object')
     expect(app.config.env.hello).toBe('bye')
   })
+
+  it('should use default main in actions', async () => {
+    process.env.WAVEORB_APP = 'test/apps/app20'
+    const app = await loader()
+    const api = app.actions.project
+    expect(typeof api.create.main).toBe('function')
+    expect(typeof api.update.main).toBe('function')
+    expect(typeof api.get.main).toBe('function')
+    expect(typeof api.find.main).toBe('function')
+    expect(typeof api.count.main).toBe('function')
+    expect(typeof api.update.main).toBe('function')
+    expect(typeof api.delete.main).toBe('function')
+    expect(typeof api.custom.main).toBe('function')
+    expect(typeof api.something.main).toBe('undefined')
+    expect(api.not.main).toEqual(false)
+  })
 })
